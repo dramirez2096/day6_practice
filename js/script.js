c689dd720744454f2d44cbfe4e9c3bf8f381131f
David Ramirez <dramirez2096@gmail.com>
2017-08-28 10:22:55 -0700

    uppercase function added

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 //First Name Button
 var firstName = document.getElementById('btn1');
 
-function uppercaseFirst(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 firstName.addEventListener('click', function(){
     var firstName = prompt("What is your first name?");
-    confirm("Is your first name " + firstName + " ?");
+    confirm("Is your first name " + firstName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}) + " ?");
 
-    console.log (firstName);
+    console.log(firstName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}));
+
+    //console.log (firstName);
 });
+
 
 //Middle Name Button
 var middleName = document.getElementsByTagName('button')[1];
 
 middleName.addEventListener('click', function(){
     var middleName = prompt("What is your middle name?");
-    confirm("Is your middle name " + middleName + " ?");
+    confirm("Is your middle name " + middleName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}) + " ?");
 
-    console.log (middleName);
+    console.log(middleName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}));
 });
+
 
 //Last Name Button
 var lastName = document.querySelector('#btn3');
 
 lastName.addEventListener('click', function(){
     var lastName = prompt("What is your last name?");
-    confirm("Is your last name " + lastName + " ?");
+    confirm("Is your last name " + lastName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}) + " ?");
 
-    console.log (lastName);
+    console.log(lastName.replace(/\b[a-z]/g,function(f){return f.toUpperCase();}));
 });
